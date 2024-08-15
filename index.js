bbd77b07b5270fea5307288b5e5164f21f8f8af0
Resul Bozburun <59255765+rbozburun@users.noreply.github.com>
2024-08-15 17:59:50 +0300

    Testing for gitleaks - secretkey=myHardcodedSecret122

--- index.js
+++ index.js
@@ -6,11 +6,15 @@ const port = 3000;
 // Hardcoded secret (for demonstration purposes only)
 const secretKey = 'myHardcodedSecret123';
 
+// Testing for gitleaks
+const secretKey2 = 'myHardcodedSecret122';
+
 app.get('/', (req, res) => {
     // Example usage of lodash
     const array = [1, 2, 3, 4, 5];
     const shuffledArray = _.shuffle(array);
     res.send(`Hello World! Here is a shuffled array: ${shuffledArray}`);
+    console.log(" Secretkey2 added: " + secretKey2);
 });
 
 app.listen(port, () => {
